fb295c2ab17521786005594a4106269159007575
Olena Kryvenko <sashakryvenko2006@gmail.com>
2024-05-27 11:53:48 +0200

    update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+          const data = createFakeContact();
+          try {
+            await fs.writeFile(PATH_DB, JSON.stringify(data), 'utf8');
+            console.log('Дані успішно записані у файл.');
+          } catch (err) {
+            console.error('Помилка запису у файл:', err);
+          }
+};
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  for (let i = 0; i < number; i++) {
+    const data = createFakeContact();
+    console.log(data);
+    try {
+      await fs.writeFile(PATH_DB, JSON.stringify(data), 'utf8');
+      console.log('Дані успішно записані у файл.');
+    } catch (err) {
+      console.error('Помилка запису у файл:', err);
+    }
+  }
+};
 
 await generateContacts(5);
